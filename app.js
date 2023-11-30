f20cd5fa7d16822139274a33337ee5ec3d49d5c9
Ritik Visapurkar <121254833+RitikVisa@users.noreply.github.com>
2023-12-01 01:39:32 +0530

    added side navbar
    used js and added google icons

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }else{
-            entry.target.classList.remove('show');
-        }
-    })
-})
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-const hiddenElement = document.querySelectorAll('.hidden');
+const hiddenElement = document.querySelectorAll(".hidden");
 hiddenElement.forEach((el) => observer.observe(el));
 
-const hiddenElement2 = document.querySelectorAll('.hidden-2');
+const hiddenElement2 = document.querySelectorAll(".hidden-2");
 hiddenElement2.forEach((el) => observer.observe(el));
+
+const cancel = document.getElementById("d");
+
+function hideSideBar() {
+  cancel.style.display = "none";
+}
+
+function showSideBar(){
+  cancel.style.display = "flex";
+
+}
